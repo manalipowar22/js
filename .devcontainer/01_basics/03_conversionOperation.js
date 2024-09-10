6009f24a99ec46aed13d244bd3754bcba627b3f1
manalipowar22 <manalipowar2226@gmail.com>
2024-09-10 05:27:37 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "manali"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+// true =>1; false =>0 
+
+
+let isLoggedIn = "manali"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0=> false
+// "" => false
+//  "manali" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
