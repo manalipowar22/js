65c51be7296d9ec33c6cf10dcb3f4b215d022346
manalipowar22 <manalipowar2226@gmail.com>
2024-09-16 14:44:41 +0000

    statements

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,80 @@
+// ************   if   ************
+const isUserLoggedIn = true
+
+// if( 2 == "2"){
+//     console.log("executed")
+// }
+
+// ************   if else  ************
+
+const temperature = 41
+
+// if( temperature === 40){
+//     console.log("less than 50");
+// } else {
+// console.log("temperature is greater than 50");
+// }
+// console.log("execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+    
+// }
+// console.log(`user power: ${power}`);
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test2");
+// don't write this type of statement
+
+
+// ************************** nesting ***********************
+
+// if(balance < 500){
+//   console.log("less than 500");
+// } else if(balance < 750){
+//     console.log("less than 750");
+
+// }else if(balance < 900){
+//     console.log("less than 900");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+    
+}
+
+if(loggedInFromGoogle || loggedInFromEmail ) {
+    console.log("user logged in");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
