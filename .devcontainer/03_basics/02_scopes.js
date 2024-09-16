080782dfd3932ab9322674d604cfe2b7f398992b
manalipowar22 <manalipowar2226@gmail.com>
2024-09-16 09:48:35 +0000

    scope

---
+++ .devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,81 @@
+// var c = 300
+  let a = 300
+if(true){
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+
+
+}
+
+//  console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+  const username = "manali"
+
+  function two(){
+    const website = "youtube"
+    // console.log(username);
+  }
+// console.log(website);
+ two()
+}
+
+one()
+// in nested functions child function access the parent function variable 
+
+
+if (true) {
+  const username = "manali"
+  if(username === "manali"){
+    const website = " youtube"
+    // console.log(username + website);
+  }
+  // console.log(website)
+}
+// console.log(username)
+
+
+// ***************************** Interesting    *********************************************************88
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+
+const addTwo = function(num){
+  return num + 2
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
